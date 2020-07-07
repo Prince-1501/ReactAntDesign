215d9f17adaa95015e1fe4182ef649f638618a8f
Prince-1501 <princekumarrpcsvm10@gmail.com>
2020-07-07 09:59:26 +0530

    commit 2nd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Row , Col} from 'antd';
+import ReactLogo from './../static/undraw.svg';
 
 function Main() {
-  return <Row style={{backgroundColor: "rgb(21, 32, 43)", height:"100vh"}}>
-  <Col
-  xs={0}
-  sm={0}
-  md={2}
-  lg={6}
-  style={{backgroundColor: "pink", height:"100vh"}}>
+  return <Row style={{width:"100vw"}}>
+
+  <Col span={12} style={{backgroundColor: "pink"}}>prince</Col>
+  <Col span={12}>
+  <img style={{width:"50vw"}} src={ReactLogo}  alt="React Logo" />
   </Col>
-  <Col
-  xs={24}
-  sm={24}
-  md={20}
-  lg={12}
-  style={{backgroundColor: "white", height:"100vh"}}>
-  </Col>
-  <Col
-  xs={0}
-  sm={0}
-  md={2}
-  lg={6}
-  style={{backgroundColor: "lightgreen", height:"100vh"}}>
-  </Col>
+
   </Row>
 }
 
